ad4dd626959be0b56b38b9db1b50d4eb9f5926cd
Flow <highflowey@gmail.com>
2024-05-21 23:17:30 +0330

    basic template

--- src/init.js
+++ src/init.js
@@ -1,9 +1,1 @@
-async function testCustomProtocol() {
-    let res = await fetch('http://app.path/')
-    console.log("Received message from custom protocol:", await res.text())
-}
-
-window.luneweb.postMessage('load', null)
-
-testCustomProtocol()
-testCustomProtocol = undefined
+console.log("Hello, LuneWeb!")
