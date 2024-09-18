3f44fb2af7c51a540eb82c2d580eebbb9ba6b36d
highflowey <highflowey@gmail.com>
2024-09-18 16:15:21 +0330

    simplifiy project

--- web/index.ts
+++ web/index.ts
@@ -1,11 +1,1 @@
-import { message } from "luneweb-api";
-import { log } from "./log";
-
-message.listen("print", (value) => {
-  log(JSON.stringify(value));
-
-  message.postMessage({
-    kind: "print",
-    value: "Received message from luau",
-  });
-});
+console.log("Page loaded");
